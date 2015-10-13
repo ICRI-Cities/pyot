d71ffcab04dbc8edb42f53dff4765774199e72ca
Mike Rosen <michael.r.rosen@intel.com>
2015-10-13 17:20:44 +0100

    Attempt #12 to fix opc fan

--- lib/pyotlib/peripherals/platform/IntelGalileo.py
+++ lib/pyotlib/peripherals/platform/IntelGalileo.py
@@ -45,6 +45,7 @@ class GalileoPlatform(peripheral.Peripheral):
     # Build function (time to create stuff)
     def build(self, params):
       self._gpio = mraa.Gpio(self.pins.getPins(self)[0]);
+      pr.Dbg("Got GPIO!");
       return;
       
     # Request function
