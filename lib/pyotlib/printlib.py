a09aac83fd9ae85e7d9205a4868e19ab1b34f6ad
Mike Rosen <michael.r.rosen@intel.com>
2015-10-15 17:15:36 +0100

    Better printing

--- lib/pyotlib/printlib.py
+++ lib/pyotlib/printlib.py
@@ -6,22 +6,23 @@
 # 12-10-2015
 #
 
+import sys
 import traceback
 
 debug = False;
 
 # Print commands
 def Msg(s):
-  print("-I- %s" % s);
+  print("%20s: -I- %s" % (sys.argv[0], s));
   return;
   
 def Wrn(s):
-  print("-W- %s" % s);
+  print("%20s: -W- %s" % (sys.argv[0], s);
   return;
   
 def Err(s):
   global debug;
-  print("-E- %s" % s);
+  print("%20s: -E- %s" % (sys.argv[0], s));
   if (debug):
     traceback.print_exc();
   return;
@@ -29,5 +30,5 @@ def Err(s):
 def Dbg(s):
   global debug;
   if (debug):
-    print("-D- %s" % s);
+    print("%20s: -D- %s" % (sys.argv[0], s));
   return;
