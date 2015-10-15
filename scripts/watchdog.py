35ccfcbd673486162b41ecb3cd0ebec693b9ae7c
Mike Rosen <michael.r.rosen@intel.com>
2015-10-15 16:33:17 +0100

    Make watchdog do stuff

--- scripts/watchdog.py
+++ scripts/watchdog.py
@@ -7,6 +7,20 @@
 #
 
 import os
+import time
+import pyotlib.printlib as pr
 
 while (True):
-  pass;
+  pr.Msg("Pinging www.intel.com to check int"
+  ret = os.system("ping -c1 -w1 www.intel.com");
+  
+  if (ret != 0):
+    pr.Err("Failed to reach www.intel.com; internet down!");
+    
+  ret = os.system("ps | grep pyot | grep -v grep ");
+  
+  if (ret != 0):
+    pr.Err("pyot process crashed!");
+    
+  pr.Msg("All is well, sleeping for 120 seconds...");
+  time.sleep(120);
