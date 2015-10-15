6946365816d8380226abe43a1f1356ded65e42d5
Mike Rosen <michael.r.rosen@intel.com>
2015-10-15 16:20:45 +0100

    Fix #6

--- main.py
+++ main.py
@@ -25,6 +25,6 @@ for f in os.listdir("scripts"):
 
 # Run all the found commands    
 for s in scripts:
-  cmd = "python scripts/" + s['script'] + " " + s['args'] + " > /dev/kmsg 2>&1 &";
+  cmd = "python scripts/" + s['script'] + " " + s['args'] + " &";
   print("-M- Running: %s" % cmd);
   os.system(cmd);
